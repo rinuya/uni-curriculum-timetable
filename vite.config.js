42582a442c0504a96afcc8b4c616d37b18113957
rinuya <rudolf.gefenitor13@gmail.com>
2023-06-11 14:21:10 +0300

    adding dist

--- vite.config.js
+++ vite.config.js
@@ -6,13 +6,7 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  // css: {
-  //   preprocessorOptions: {
-  //     scss: {
-  //       additionalData: "@import '@/assets/main.scss';"
-  //     }
-  //   }
-  // },
+  base: '/uni-curriculum-timetable',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
